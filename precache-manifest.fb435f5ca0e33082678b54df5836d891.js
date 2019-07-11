8134d53d506f0358b0e04d0bcd2dcf786e9e6a3d
David Schach <schachdavid@web.de>
2019-07-11 18:33:15 +0200

    Updates

---
+++ precache-manifest.fb435f5ca0e33082678b54df5836d891.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32de3ff052d6e83516d5899d310f36e3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "da9a055e3d2d757c4a7a",
+    "url": "/portfolio/static/css/main.d6310c79.chunk.css"
+  },
+  {
+    "revision": "593d122a04acaac66f6c",
+    "url": "/portfolio/static/js/2.2c7cd9af.chunk.js"
+  },
+  {
+    "revision": "da9a055e3d2d757c4a7a",
+    "url": "/portfolio/static/js/main.d24300a3.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "cf6202937ed1de278453431afd2d1d9b",
+    "url": "/portfolio/static/media/decision_maker.cf620293.png"
+  },
+  {
+    "revision": "f3cf6f75b4816ff35150a0590d857319",
+    "url": "/portfolio/static/media/happy2help.f3cf6f75.PNG"
+  }
+]);
